60a36e39fc661535bba4898b4e479f38a8fc1a91
md.works <mazdze@gmail.com>
2015-06-30 18:16:37 +0300

    Zhe1ka

--- js/commonJs.js
+++ js/commonJs.js
@@ -10,4 +10,35 @@ $(document).ready(function(){
             });
         }
     });
+
+    $('body').append('<div class="place-for-popup"></div>');
+    $('.place-for-popup').load('popup.html .wrap-all-form', function(){
+        function popup(popup, btnClick) {
+            btnClick.on('click', function (e) {
+                e.preventDefault();
+
+                popup.fadeIn(300);
+                $('.bg-popup').fadeIn(300);
+            });
+
+            $('.bg-popup').on('click', function (e) {
+                e.stopPropagation();
+
+                popup.fadeOut(300);
+                $('.bg-popup').fadeOut(300);
+            });
+
+            popup.on('click', function (e) {
+                e.stopPropagation();
+            });
+        }
+
+        var popup1 = $('#popup1'),
+            popup2 = $('#popup2'),
+            btn1 = $('#btn-popup1-js'),
+            btn2 = $('#btn-popup2-js');
+
+        popup(popup1, btn1);
+        popup(popup2, btn2);
+    });
 });
