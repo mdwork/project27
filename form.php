4450528cf91586a008b777d1551c92814e720cd1
md.works <mazdze@gmail.com>
2015-07-02 14:22:45 +0300

    Zhe1ka

--- form.php
+++ form.php
@@ -1,12 +1,30 @@
 <?php
-    $nameUser = trim(strip_tags($_POST['nameUser']));
-    $tellUser = trim(strip_tags($_POST['tellUser']));
-    $mailUser = trim(strip_tags($_POST['mailUser']));
-    $messageUser = trim(strip_tags($_POST['messageUser']));
+    $message;
+    
+    if($_POST['nameUser'] != '' || $_POST['nameUser'] != ' ') {
+        $nameUser = trim(strip_tags($_POST['nameUser']));
+
+        $message = "Имя отправителя: $nameUser";
+    }
+    if($_POST['tellUser']) {
+        $tellUser = trim(strip_tags($_POST['tellUser']));
+
+        $message = "Имя отправителя: $nameUser\nНомер телефона: $tellUser";
+    }
+    if($_POST['mailUser']) {
+        $mailUser = trim(strip_tags($_POST['mailUser']));
+
+        $message = "Имя отправителя: $nameUser\nНомер телефона: $tellUser\nemail: $mailUser";
+    }
+    if($_POST['messageUser']) {
+        $messageUser = trim(strip_tags($_POST['messageUser']));
+
+        $message = "Имя отправителя: $nameUser\nНомер телефона: $tellUser\nemail: $mailUser\nСообщение: $messageUser";
+    }
+    
 
     $from = 'mazdze@gmail.com';
     $subject = 'Форма обратной связи "Онлайн займы"';
-    $message = "Имя отправителя: $nameUser \nНомер телефона: $tellUser";
     $fromName = "Форма";
     $headers = "Return-Path: <".$fromName.">\r\n";
     $headers .= "From: ".$fromName." <".$from.">\r\n";
